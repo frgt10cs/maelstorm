e095b75f27e3e692d9662ee7b879b3d03e5ca88d
frgt10 <ivan_gavrilov_2013@mail.ru>
2020-04-05 16:53:00 +0300

    start working with js aes

---
+++ Maelstorm/wwwroot/js/modules/cryptoModule.js
@@ -0,0 +1,52 @@
+﻿var crypto = (function () {
+    var _encoding;
+
+    var validatePassphrase = function (passphrase, length) {
+        var requiredLength = length / 8;
+        if (passphrase.length < requiredLength)
+            passphrase = passphrase + passphrase.substring(0, requiredLength - passphrase.length);
+        else if (passphrase.length > requiredLength)
+            passphrase = passphrase.substring(0, requiredLength);
+        return passphrase;
+    }
+
+    return {
+        init: function (encoding) {
+            _encoding = encoding;
+        },
+
+        generateIV() {
+
+        },
+
+        genereateAesKeyByPassPhrase(passphrase, length) {
+            passphrase = validatePassphrase(passphrase, length);
+            var keyBytes = getBytes(passphrase);
+            return window.crypto.subtle.importKey(
+                "raw",
+                keyBytes,
+                "AES-CBC",
+                true,
+                ["encrypt", "decrypt"]
+            );
+        },
+
+        encryptAes(aesKey, iv, plainText) {
+            var dataBytes = _encoding.getBytes(plainText);
+            return window.crypto.subtle.encrypt({
+                name: "AES-CBC",                
+                iv: iv,
+            },
+            aesKey, dataBytes);
+        },
+
+        decryptAes(aesKey, iv, encryptedDataBase64) {
+            var encryptedBytes = _encoding.base64ToArray(encryptedDataBase64);
+            return window.crypto.subtle.decrypt({
+                name: "AES-CBC",
+                iv: iv,
+            },
+            aesKey, encryptedBytes);
+        }       
+    }
+})();
---
+++ Maelstorm/wwwroot/js/modules/encodingModule.js
@@ -0,0 +1,39 @@
+﻿var encoding = (function () {
+    return {
+        getBytes: function (string) {
+            string = btoa(unescape(encodeURIComponent(string))),
+                charList = string.split(''),
+                uintArray = [];
+            for (var i = 0; i < charList.length; i++) {
+                uintArray.push(charList[i].charCodeAt(0));
+            }
+            return new Uint8Array(uintArray);
+        },
+
+        getString: function (uintArray) {
+            var encodedString = String.fromCharCode.apply(null, uintArray),
+                decodedString = decodeURIComponent(escape(atob(encodedString)));
+            return decodedString;
+        },
+
+        arrayToBase64: function (buffer) {
+            var binary = '';
+            var bytes = new Uint8Array(buffer);
+            var len = bytes.byteLength;
+            for (var i = 0; i < len; i++) {
+                binary += String.fromCharCode(bytes[i]);
+            }
+            return window.btoa(binary);
+        },
+
+        base64ToArray: function(base64) {
+            var binaryString = window.atob(base64);
+            var len = binaryString.length;
+            var bytes = new Uint8Array(len);
+            for (var i = 0; i < len; i++) {
+                bytes[i] = binaryString.charCodeAt(i);
+            }
+        return new Uint8Array(bytes.buffer);
+        }    
+    }
+})();
